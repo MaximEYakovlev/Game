7810ca071bde76bb4ccc242a942a2e8c3287f0c5
Maxim Yakovlev <it.trainee@yandex.ru>
2022-08-25 02:13:32 +0300

    created class

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 const grid = document.querySelector('.grid')
+const blockWidth = 100
+const blockHeight = 20
 
-const block = document.createElement('div')
-block.classList.add('block')
-grid.appendChild(block) 
+class Block {
+    constructor(xAxis, yAxis) {
+        this.bottomLeft = [xAxis, yAxis]
+        this.bottomRight = [xAxis + blockWidth, yAxis]
+        this.topLeft = [xAxis, yAxis + blockHeight]
+        this.topRight = [xAxis + blockWidth, yAxis + blockHeight]
+    }
+}
+
+const addBlock = () => {
+    const block = document.createElement('div')
+    block.classList.add('block')
+    block.style.left = '100px'
+    block.style.bottom = '50px'
+    grid.appendChild(block)
+}
+
+addBlock()
+
