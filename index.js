98baa6832130f24f47d213f5442b5b11ada439c4
Maxim Yakovlev <it.trainee@yandex.ru>
2022-08-26 22:35:00 +0300

    created all the blocks

--- index.js
+++ index.js
@@ -12,7 +12,21 @@ class Block {
 }
 
 const blocks = [
-    new Block(10, 270)
+    new Block(10, 270),
+    new Block(120, 270),
+    new Block(230, 270),
+    new Block(340, 270),
+    new Block(450, 270),
+    new Block(10, 240),
+    new Block(120, 240),
+    new Block(230, 240),
+    new Block(340, 240),
+    new Block(450, 240),
+    new Block(10, 210),
+    new Block(120, 210),
+    new Block(230, 210),
+    new Block(340, 210),
+    new Block(450, 210),
 ]
 
 const addBlocks = () => {
